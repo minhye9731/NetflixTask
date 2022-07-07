af2170ed933aa525dd2655893ea8faf60f585c77
minhye9731 <jenney62639731@gmail.com>
2022-07-07 09:12:12 +0900

    Initial Commit

---
+++ NetflixTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixTask
+//
+//  Created by 강민혜 on 7/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
