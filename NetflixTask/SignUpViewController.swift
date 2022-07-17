b9585a6877e288960ee7106b5ac6860eeb4520ff
minhye9731 <jenney62639731@gmail.com>
2022-07-18 01:11:02 +0900

    - 로그인 alert 적용 - 로그인 성공시 화면전환 적용 - TextField, keyboard에 라이브러리 적용

--- NetflixTask/SignUpViewController.swift
+++ NetflixTask/SignUpViewController.swift
@@ -19,14 +19,21 @@ class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.systemBackground
-        signUpButtonSetting()
+        setUI()
         settingSwitch()
+    }
+    
+    // MARK: - UI설정
+    func setUI() {
+        view.backgroundColor = UIColor.systemBackground
+        
         designTextField(emailTextField, keyboardType: .emailAddress, placeholder: "이메일 주소 또는 전화번호", isSecureTextEntry: false)
         designTextField(passwordTextField, keyboardType: .asciiCapable, placeholder: "비밀번호", isSecureTextEntry: true)
         designTextField(nickNameTextFIeld, keyboardType: .default, placeholder: "닉네임", isSecureTextEntry: false)
         designTextField(locationTextField, keyboardType: .default, placeholder: "위치", isSecureTextEntry: false)
         designTextField(recCodeTextField, keyboardType: .numberPad, placeholder: "추천 코드 입력", isSecureTextEntry: false)
+        
+        signUpButtonSetting()
     }
     
     func designTextField(_ textFieldName: UITextField, keyboardType: UIKeyboardType, placeholder: String, isSecureTextEntry: Bool) {
@@ -41,7 +48,6 @@ class SignUpViewController: UIViewController {
         textFieldName.isSecureTextEntry = isSecureTextEntry
     }
 
-    // MARK: - 회원가입 버튼 속성
     func signUpButtonSetting() {
         signUpButton.layer.cornerRadius = 8
         signUpButton.clipsToBounds = true
@@ -63,10 +69,13 @@ class SignUpViewController: UIViewController {
         if self.onOffSwitch.isOn { return }
         // switch를 ON으로 유지해놓음
         self.onOffSwitch.setOn(true, animated: true)
+        
         let alert = UIAlertController(title: "현재 해당 기능을 끄시겠습니까?", message: "블라블라", preferredStyle: .alert)
+        
         // alert에서의 cancel 클릭시
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
             print("해당 기능 끄기를 취소하셨습니다. \n 현재 기능을 그대로 유지합니다.")}))
+        
         // alert에서의 ok 클릭시
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { _ in
             // 상태를 off로 변경함
@@ -79,7 +88,7 @@ class SignUpViewController: UIViewController {
         view.endEditing(true)
     }
 
-    // MARK: - 회원가입 버튼 클릭시 동작
+    // MARK: - 회원가입 버튼 클릭시 액션
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
         if emailTextField.text == "" || passwordTextField.text == "" {
             self.presentAlert(title: "이메일과 비밀번호를 모두 입력해주세요.")
@@ -88,7 +97,17 @@ class SignUpViewController: UIViewController {
             self.presentAlert(title: "비밀번호는 6자리 이상으로 설정해주세요.")
             return
         }
-        self.presentAlert(title: "회원가입이 성공적으로 완료되었습니다 :)")
-        view.endEditing(true)
+        self.signupAlert(title: "<안내>", message: "회원가입이 성공적으로 완료되었습니다 :)", preferredStyle: .alert) {_ in
+            self.view.endEditing(true)
+            self.moveToMain()
+        }
+    }
+    
+    // 화면이동
+    func moveToMain() {
+        let newVC = self.storyboard?.instantiateViewController(identifier: "HomeViewController")
+        newVC?.modalTransitionStyle = .crossDissolve
+        newVC?.modalPresentationStyle = .fullScreen
+        self.present(newVC!, animated: true, completion: nil)
     }
 }
